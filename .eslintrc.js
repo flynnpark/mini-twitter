935bab283ecba5843c67d22e69503726c12b876b
Flynn <wphestiraid@gmail.com>
2023-04-11 10:09:12 +0900

    :sparkles: 회원가입 API 구현

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'prettier'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Side effect imports.
+          ['^\\u0000'],
+          // Node.js builtins prefixed with `node:`.
+          ['^node:'],
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^@?\\w', '^(libs|pages)(/.*|$)'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ['^'],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
+  },
+};
---
+++ pages/api/users/index.ts
@@ -0,0 +1,49 @@
+import createHttpError from 'http-errors';
+import { NextApiRequest, NextApiResponse } from 'next';
+import client from 'libs/server/db';
+import withHandler, { CommonResponseType } from 'libs/server/withHandler';
+
+interface CreateUserRequest extends NextApiRequest {
+  body: {
+    email: string;
+    name: string;
+  };
+}
+
+async function createUser(
+  req: CreateUserRequest,
+  res: NextApiResponse<CommonResponseType>
+) {
+  const { email, name } = req.body;
+  if (!email || !name) {
+    throw new createHttpError.BadRequest('올바르지 않은 요청입니다');
+  }
+
+  const existingUser = await client.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (existingUser) {
+    throw new createHttpError.BadRequest('이미 가입된 이메일입니다');
+  }
+
+  const newUser = await client.user.create({
+    data: {
+      email,
+      name,
+    },
+  });
+
+  return res.status(201).json({
+    success: true,
+    user: {
+      email: newUser.email,
+      name: newUser.name,
+    },
+  });
+}
+
+export default withHandler({
+  POST: { handler: createUser },
+});
